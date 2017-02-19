e52dd90a89763dba506d96f927c87a76d975fbcb
YaraA-Hakiim <yara.ahakiim@gmail.com>
2017-02-19 17:26:45 +0200

    Add login and register UIs

---
+++ app/src/main/java/com/example/yara/cattlemonitor/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.example.yara.cattlemonitor;
+
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    AppCompatButton btnLogin;
+    EditText editTextEmail;
+    EditText editTextPassword;
+    TextView registerLink;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initializeViews();
+
+    }
+
+    public void initializeViews ()
+    {
+        btnLogin =(AppCompatButton) findViewById(R.id.btnLogin);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        registerLink = (TextView) findViewById(R.id.registerLink);
+    }
+
+
+}
