b423c2f76ebab9d9ef83afa4e4f14fd6054e5afe
YaraA-Hakiim <yara.ahakiim@gmail.com>
2017-04-09 17:30:25 +0200

    Added registeration code

--- app/src/main/java/com/example/yara/cattlemonitor/AddUserActivity.java
+++ app/src/main/java/com/example/yara/cattlemonitor/AddUserActivity.java
@@ -1,16 +1,32 @@
 package com.example.yara.cattlemonitor;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.EditText;
-public class AddUserActivity extends AppCompatActivity {
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AddUserActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText editTextName ;
     EditText editTextEmail ;
     EditText editTextPassword ;
     EditText editTextConfirmPassword ;
     EditText editTextPhoneNumber ;
+    AppCompatButton register;
+
+    User user;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +35,30 @@ public class AddUserActivity extends AppCompatActivity {
 
         initializeViews();
 
-        validate();
 
+
+    }
+
+    public void registerUser()
+    {
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(validate()) {
+
+            progressDialog.setMessage("Registering Please Wait...");
+            progressDialog.show();
+
+            firebaseAuth.createUserWithEmailAndPassword(editTextEmail.getText().toString(), editTextPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful())
+                                Toast.makeText(getApplicationContext(), "Registered", Toast.LENGTH_LONG).show();
+                            else if (!task.isSuccessful())
+                                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                        }
+                    });
+        }
     }
 
     public void initializeViews()
@@ -30,6 +68,15 @@ public class AddUserActivity extends AppCompatActivity {
         editTextPassword =(EditText) findViewById(R.id.editTextPassword);
         editTextConfirmPassword =(EditText) findViewById(R.id.editTextConfirmPassword);
         editTextPhoneNumber =(EditText) findViewById(R.id.editTextPhoneNumber);
+
+        register = (AppCompatButton) findViewById(R.id.btn_signup);
+        register.setOnClickListener(this);
+        progressDialog = new ProgressDialog(this);
+    }
+
+    public void fillUserInfo()
+    {
+        user = new User();
     }
 
     boolean validate ()
@@ -58,6 +105,11 @@ public class AddUserActivity extends AppCompatActivity {
             isValid = false;
         }
 
+        if (password.length()<6)
+        {
+            editTextPassword.setError("Password must be at least 6 chars");
+        }
+
         if(confirmPassword.isEmpty() || !confirmPassword.equals(password))
         {
             editTextConfirmPassword.setError("Passwords must match");
@@ -73,6 +125,12 @@ public class AddUserActivity extends AppCompatActivity {
 
 
         return isValid;
+    }
+
+    @Override
+    public void onClick(View view) {
+        //calling register method on click
+        registerUser();
     }
 
 
---
+++ app/src/main/java/com/example/yara/cattlemonitor/User.java
@@ -0,0 +1,30 @@
+package com.example.yara.cattlemonitor;
+
+/**
+ * Created by YaRa on 4/9/2017.
+ */
+
+public class User
+{
+    String name;
+    String phoneNumber;
+    String farmName;
+    String type;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setFarmName(String farmName) {
+        this.farmName = farmName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
